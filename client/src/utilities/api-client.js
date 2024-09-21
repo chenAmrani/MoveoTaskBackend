0a268798fdcae5293e7e75f941ec03d51b5e8e88
chenAmrani <chenamrani5@gmail.com>
2024-09-21 16:27:29 +0300

    build front

--- client/src/utilities/api-client.js
+++ client/src/utilities/api-client.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 var socket;
 export var initSocketConnection = function () {
-    socket = io('https://moveo-task-pink.vercel.app');
+    socket = io('https://moveo-task-bice.vercel.app');
     return socket;
 };
