c9b1e822a8652bef5d0cc7158be6868544ccb346
chenAmrani <chenamrani5@gmail.com>
2024-09-21 16:19:01 +0300

    update

--- server/models/codeBlock.ts
+++ server/models/codeBlock.ts
@@ -1,18 +1,18 @@
 import { Schema, model, Document } from 'mongoose';
 
 // Interface for CodeBlock document
-export interface CodeBlockDocument extends Document {
+export interface ICodeBlock {
   blockTitle: string;
   blockCode: string;
   correctSolution: string;
 }
 
 // Schema definition for the CodeBlock model
-const CodeBlockSchema = new Schema<CodeBlockDocument>({
+const CodeBlockSchema = new Schema<ICodeBlock>({
   blockTitle: { type: String, required: true },
   blockCode: { type: String, required: true },
   correctSolution: { type: String, required: true },
 });
 
 // Export the model
-export default model<CodeBlockDocument>('CodeBlock', CodeBlockSchema);
+export default model<ICodeBlock>('CodeBlock', CodeBlockSchema);
