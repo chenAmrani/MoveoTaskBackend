8ae7783cf8f522bc60647d115fd8732a8e98f9cb
chenAmrani <chenamrani5@gmail.com>
2024-09-23 18:05:44 +0300

    update url

--- client/src/pages/LobbyPage.tsx
+++ client/src/pages/LobbyPage.tsx
@@ -16,7 +16,7 @@ const LobbyPage: React.FC = () => {
   useEffect(() => {
     const fetchCodeBlocks = async () => {
       try {
-        const response = await axios.get<CodeBlock[]>('https://moveo-task-seven.vercel.app/codeblocks'); 
+        const response = await axios.get<CodeBlock[]>('https://moveo-task-six.vercel.app/codeblocks'); 
         console.log("Response data:", response.data);
         setCodeBlocks(response.data);
         console.log("CodeBlocks!!!!:", codeBlocks);
