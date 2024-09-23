dbb0362e6384e7f8328693271f440c541eb9b990
chenAmrani <chenamrani5@gmail.com>
2024-09-23 21:38:21 +0300

    move client directory to another repo

--- client/src/utilities/api-client.ts
+++
@@ -1,8 +0,0 @@
-import { io, Socket } from 'socket.io-client';
-
-let socket: Socket;
-
-export const initSocketConnection = () => {
-     socket = io(`https://moveo-task-six.vercel.app`); 
-  return socket;
-};
