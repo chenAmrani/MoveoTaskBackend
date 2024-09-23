99d617845f3475b5366c2ab63e3e5c5a288aa880
chenAmrani <chenamrani5@gmail.com>
2024-09-23 20:05:38 +0300

    update url

--- client/src/utilities/api-client.ts
+++ client/src/utilities/api-client.ts
@@ -3,6 +3,6 @@ import { io, Socket } from 'socket.io-client';
 let socket: Socket;
 
 export const initSocketConnection = () => {
-     socket = io(`https://moveo-task-eight.vercel.app`); 
+     socket = io(`https://moveo-task-six.vercel.app`); 
   return socket;
 };
