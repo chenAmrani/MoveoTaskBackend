8b2d8effca564d6fbf5dd970ec948cc1f0959b81
chenAmrani <chenamrani5@gmail.com>
2024-09-25 15:20:55 +0300

    u

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,16 +4,13 @@ import express, { Express } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import codeBlockRoutes from './routes/codeBlock_Route';
-
-require('dotenv').config(); 
-
+import codeBlockRoutes from "./routes/codeBlock_Route";
 
 const initApp = (): Promise<Express> => {
   const promise = new Promise<Express>((resolve) => {
     const db = mongoose.connection;
     db.once("open", () => console.log("Connected to Database"));
-    db.on("error", (error: Error) => console.error(error));
+    db.on("error", (error) => console.error(error));
     const url = process.env.DB_URL;
     mongoose.connect(url!).then(() => {
       const app = express();
@@ -21,11 +18,12 @@ const initApp = (): Promise<Express> => {
       app.use(cors({ origin: '*' }));
       app.use(bodyParser.json());
       app.use(bodyParser.urlencoded({ extended: true }));
+      // Routes setup
       app.use("/codeblocks", codeBlockRoutes);
+
       resolve(app);
-      });
-      
     });
+  });
   return promise;
 };
 
